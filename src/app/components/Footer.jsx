78accdf1a11827ea57b6d0ffec93da6e58ee0e90
Onion Lim <lichonglim@gmail.com>
2024-04-21 23:30:36 +0800

    replaced the single quote with &apos;

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <footer className='footer border border-t-[#33353F] z-10 border-l-transparent border-r-transparent text-white'>
-        <div className='container p-12 flex justify-between'>
-            <span>LOGO</span>
-            <p className='text-slate-600'>All rights reserved.</p>
-        </div></footer>
-  )
-}
+    <footer className="footer border border-t-[#33353F] z-10 border-l-transparent border-r-transparent text-white">
+      <div className="container p-12 flex justify-between">
+        <span>LOGO</span>
+        <p className="text-slate-600">All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
